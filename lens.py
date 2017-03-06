da921d9ad853cc1ed54df302ee64c438c8b2045c
Louis Pham <l.c.pham@hotmail.com>
2017-03-06 04:21:51 -0500

    changed output filenames to use out_file_root

--- lens.py
+++ lens.py
@@ -14,10 +14,10 @@ out_file_root = "test_file_stem"
 field_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_field.fits"
 halo_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_halo.fits"
 unlensed_primary_file = "FromNERSC/ffp10_unlensed_scl_cmb_000_alm.fits"
-#output filenames
+#phi output filenames
 comb_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_comb.fits"
-phi_alm_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_alm.fits"
-phi_map_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_map.fits"
+phi_alm_file = "kappa_maps/" + out_file_root + "_n4096_lmax5120_phi_alm.fits"
+phi_map_file = "kappa_maps/" + out_file_root + "_n4096_lmax5120_phi_map.fits"
 
 nside, lmax = kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, comb_kappa_file, phi_alm_file, writeMap=True, phi_map_file=phi_map_file)
 

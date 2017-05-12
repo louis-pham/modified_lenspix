11f81a6d6f2fc2fcd99a22ef98d7e66ebcdf4508
Louis Pham <l.c.pham@hotmail.com>
2017-05-12 11:03:18 -0400

    Further changes to make lens.py easier to use

--- kappa2phi.py
+++ kappa2phi.py
@@ -2,29 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import healpy as hp
 
-def kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, phi_alm_file, writeMap=False, phi_map_file=None):
+def kap2phi(field_kappa, halo_kappa, unlensed_primary, phi_alm_file, writeMap=False, phi_map_file=None):
     print "===============kappa2phi==============="
-    print "----Loading maps..."
-    field_kappa_map = hp.read_map(field_kappa_file)
-    nside = hp.get_nside(field_kappa_map)
-    halo_kappa_map = hp.read_map(halo_kappa_file) 
-    print "----Done."
-
-    print "----Loading primary..."
-    unlensed_primary = hp.read_alm(unlensed_primary_file)
+    #field_kappa = hp.read_map(field_kappa_file)
+    nside = hp.get_nside(field_kappa)    
+    #print "Loading halo kappa..."
+    #halo_kappa = hp.read_map(halo_kappa_file) 
+    #print "Loading primary..."
+    #unlensed_primary = hp.read_alm(unlensed_primary_file)
     lmax = hp.Alm.getlmax(len(unlensed_primary))
-    print "----Done."
-    
+    #print "----Done loading maps."
+
     #combine
-    print "----Combining maps..." #convert to alm before combining?
-    kappa_map = field_kappa_map + halo_kappa_map
-    print "----Done."
+    print "----Combining field and halo kappa..."
+    kappa_map = field_kappa + halo_kappa
+    print "----Done combining."
     #print "----Writing new combined map to file..."
     #hp.write_map(comb_kappa_file, kappa_map)
     #print "----Done."
-    
+
     #no field kappa right now, so just override
-    #kappa_map = halo_kappa_map
+    #kappa_map = halo_kappa
     
     #convert to alm
     print "----Converting kappa map to alm..."
@@ -36,16 +34,17 @@ def kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, phi_alm_fi
     l,m = hp.Alm.getlm(lmax)
     phi_lm = kappa_lm * (2.0 / (l*(l+1.0)))
     phi_lm[l==0] = 0
-    
+
     print "----Writing phi alm to file..."
+    #print phi_alm_file
+    #print phi_lm
     hp.write_alm(phi_alm_file, phi_lm)
-    print "----Done."
-    #convert to map
-    print "----Converting phi to map..."
-    phi_map = hp.alm2map(phi_lm, nside, lmax=lmax)
     print "----Done."
 
     if writeMap and phi_map_file:
+        #convert to map
+        print "----Converting phi to map..."
+        phi_map = hp.alm2map(phi_lm, nside, lmax=lmax)
         print "----Writing phi map to file..."
         hp.write_map(phi_map_file, phi_map)
         print "----Done."
@@ -56,11 +55,12 @@ def kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, phi_alm_fi
 if __name__ == "__main__":
     #todo: file selection
     #input filenames
-    field_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_field.fits"
-    halo_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_halo.fits"
-    unlensed_primary_file = "FromNERSC/ffp10_unlensed_scl_cmb_000_alm.fits"
+    #field_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_field.fits"
+    #halo_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_halo.fits"
+    #unlensed_primary_file = "FromNERSC/ffp10_unlensed_scl_cmb_000_alm.fits"
     #output filenames
-    comb_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_comb.fits"
-    phi_alm_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_alm.fits"
-    phi_map_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_map.fits"
-    kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, comb_kappa_file, phi_alm_file, writeMap=True, phi_map_file=phi_map_file)
+    #comb_kappa_file = "kappa_maps/8Gpc_n4096_nb23_nt18_kap_comb.fits"
+    #phi_alm_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_alm.fits"
+    #phi_map_file = "kappa_maps/comb_z_4.6_n4096_lmax5120_phi_map.fits"
+    #kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, comb_kappa_file, phi_alm_file, writeMap=True, phi_map_file=phi_map_file)
+    pass
--- lens.py
+++ lens.py
@@ -5,67 +5,101 @@ from astropy.io import fits
 import subprocess
 import argparse
 
+def checkFitsType(fileName):
+    '''
+    Check if FITS file is a map or alm.
+    '''
+    hdulist = fits.open(fileName)
+    try:
+        nside = hdulist[1].header['NSIDE']
+        fitsType = 'map'
+    except KeyError:
+        #assume alm
+        fitsType = 'alm'
+    except Exception:
+        raise
+    hdulist.close()
+    return fitsType
+
+def appendToFilename(filename, newStr):
+    return "{0}_{2}.{1}".format(*filename.rsplit('.', 1) + [newStr])
+
+#at simlens step, will need a phi and primary alm -- check inputs here
 parser = argparse.ArgumentParser(description='Lens CMB primary.')
-parser.add_argument('input_field_kappa', help='filename of the field kappa file (HEALPix map)')
+parser.add_argument('input_field_kappa', help='filename of the field kappa file (pref. HEALPix map)')
 #parser.add_argument('input_halo_kappa', help='filename of the halo kappa file (HEALPix map)')
-parser.add_argument('input_primary', help='filename of the unlensed primary file (HEALPix alm)')
+parser.add_argument('input_primary', help='filename of the unlensed primary file (pref. HEALPix alm)')
 parser.add_argument('output_phi', help='filename of the outputted phi file (HEALPix alm)')
 parser.add_argument('output_lensed', help='filename of the outputted lensed file (HEALPix map)')
 parser.add_argument('-ol', '--output_lmax', help='new lmax for the lensed map')
 args = parser.parse_args()
 
-generic_params = 'generic_params.ini'
-specific_params = 'specific_params.ini'
-out_file_root = "may9_test"
+genericParams = 'generic_params.ini'
+specificParams = '/scratch2/r/rbond/phamloui/lenspix_files/output/specific_params.ini' #cant save to home directory when running this script as a job
+outFileRoot = "/scratch2/r/rbond/phamloui/lenspix_files/output/may12_test" #used mainly for power spectra files - in the future just append _power to filenames from arguments?
 
 #input filenames
-#field_kappa_file = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/8Gpc_n2048_nb23_nt18_kap_halo.fits"
-#halo_kappa_file = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/8Gpc_n2048_nb23_nt18_kap_halo.fits"
-#unlensed_primary_file = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/cib_fullsky_ns2048_zmin0.0_zmax1.245_nu217_13579_normalized_alm.fits"
-field_kappa_file = args.input_field_kappa
-#create a zero map and use that in place of halo kappa, since we only have field kappa now
-#hdulist = fits.open(field_kappa_file)
-#nside = hdulist[1].header['NSIDE']
-#hdulist.close()
-#zero_map = np.zeros(12 * (nside**2)) 
-#hp.write_map("zeros.fits", zero_map)
-halo_kappa_file = "zeros.fits"
-unlensed_primary_file = args.input_primary
+#fieldKappaFile = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/8Gpc_n2048_nb23_nt18_kap_halo.fits"
+#haloKappaFile = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/8Gpc_n2048_nb23_nt18_kap_halo.fits"
+#nlensedPrimaryFile = "/scratch2/r/rbond/phamloui/lenspix/kappa_maps/cib_fullsky_ns2048_zmin0.0_zmax1.245_nu217_13579_normalized_alm.fits"
+fieldKappaFile = args.input_field_kappa
+#aloKappaFile = args.input_halo_kappa
+unlensedPrimaryFile = args.input_primary
 
-#output filenames
-#comb_kappa_file = "kappa_maps/" + out_file_root + "8Gpc_n2048_nb23_nt18_kap_comb.fits"
-#phi_alm_file = "kappa_maps/" + out_file_root + "_n2048_phi_alm.fits"
-#phi_map_file = "kappa_maps/" + out_file_root + "_n2048_phi_map.fits"
-phi_alm_file = args.output_phi
-lensed_file = args.output_lensed
-lmax = kap2phi(field_kappa_file, halo_kappa_file, unlensed_primary_file, phi_alm_file)
+#output filenames                                                                                    
+#combKappaFile = outFileRoot + "8Gpc_n2048_nb23_nt18_kap_comb.fits"             
+#phiAlmFile = outFileRoot + "_n2048_phi_alm.fits"                                
+#phiMapFile = outFileRoot + "_n2048_phi_map.fits"                                 
+phiAlmFile = args.output_phi
+lensedFile = args.output_lensed
 
+#get nside
+hdulist = fits.open(fieldKappaFile)
+nside = hdulist[1].header['NSIDE']
+hdulist.close()
+
+#zeroMap = np.zeros(12 * (nside**2)) #create a zero map and use that in place of halo kappa, since we only have field kappa now
+#hp.write_map("zeros.fits", zeroMap)
+haloKappaFile = "zeros.fits" #for now safe to assume zeros.fits will be same nside as other inputs
+
+#load maps
+print "Loading maps..."
+fieldKappaType = checkFitsType(fieldKappaFile)
+primaryType = checkFitsType(unlensedPrimaryFile)
+fieldKappa = hp.read_map(fieldKappaFile)
+haloKappa = hp.read_map('zeros.fits')
+if primaryType != 'alm':
+    print "Primary is map - converting to alm..."
+    unlensedPrimaryMap = hp.read_map(unlensedPrimaryFile)
+    unlensedPrimary = hp.map2alm(unlensedPrimaryMap)
+    unlensedPrimaryFile = appendToFilename(unlensedPrimaryFile, "alm")
+    hp.write_alm(unlensedPrimaryFile, unlensedPrimary)
+    print "Saved new primary alm to", unlensedPrimaryFile
+else:
+    unlensedPrimary = hp.read_alm(unlensedPrimaryFile)
+#get lmax and create phi alm
+lmax = kap2phi(fieldKappa, haloKappa, unlensedPrimary, phiAlmFile)
+#lmax = kap2phi(fieldKappaFile, haloKappaFile, unlensedPrimaryFile, phiAlmFile)
 print 'Obtained parameters NSIDE:', nside, 'and LMAX:', lmax
 
-#todo: use nside and lmax from above
-#comb_kappa_file = "kappa_maps/8Gpc_" + nside + "_nb23_nt18_kap_comb.fits"
-#phi_alm_file = "kappa_maps/" + out_file_root + "_" + nside + "_lmax" + lmax + "_phi_alm.fits"
-#phi_map_file = "kappa_maps/" + out_file_root + "_" + nside + "_lmax" + lmax + "_phi_map.fits"
-
 print 'Creating params file...'
-
 if args.output_lmax:
-    output_lmax = args.output_lmax
-    print 'Output lmax set to', str(output_lmax) + '.'
+    outputLmax = args.output_lmax
+    print 'Output lmax set to', str(outputLmax) + '.'
 else:
-    output_lmax = lmax
-    print 'Output lmax defaulted to input lmax', str(output_lmax) + '.'
+    outputLmax = lmax
+    print 'Output lmax defaulted to input lmax', str(outputLmax) + '.'
 
-subprocess.call(['cp', generic_params, specific_params])
+subprocess.call(['cp', genericParams, specificParams])
 #commas are used as delimiters for sed commands to avoid escaping (back)slashes -- this assumes there's no commas in the filenames
-subprocess.call(['sed', '-i', 's,__NSIDEREPLACE__,' + str(nside) + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__LMAXREPLACE__,' + str(lmax) + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__OUTFILEROOTREPLACE__,' + out_file_root + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__OUTPUTLMAXREPLACE__,' + str(output_lmax) + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__PRIMARYFILEREPLACE__,' + unlensed_primary_file + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__PHIFILEREPLACE__,' + phi_alm_file + ',g', specific_params])
-subprocess.call(['sed', '-i', 's,__LENSEDFILEREPLACE__,' + lensed_file + ',g', specific_params])
+subprocess.call(['sed', '-i', 's,__NSIDEREPLACE__,' + str(nside) + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__LMAXREPLACE__,' + str(lmax) + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__OUTFILEROOTREPLACE__,' + outFileRoot + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__OUTPUTLMAXREPLACE__,' + str(outputLmax) + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__PRIMARYFILEREPLACE__,' + unlensedPrimaryFile + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__PHIFILEREPLACE__,' + phiAlmFile + ',g', specificParams])
+subprocess.call(['sed', '-i', 's,__LENSEDFILEREPLACE__,' + lensedFile + ',g', specificParams])
 print 'Params file created.'
 print 'Running simlens...'
-subprocess.call(['mpirun', '-np', '24', './simlens', specific_params])
+subprocess.call(['mpirun', '-np', '24', './simlens', specificParams])
 print 'Simlens complete.'
